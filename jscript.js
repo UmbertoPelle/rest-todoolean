38bbd49e58b8e0ac39ea7cc0b23452426621cc01
Umberto <umbertopellegrino96@gmail.com>
2020-08-26 15:04:42 +0200

    getList

--- jscript.js
+++ jscript.js
@@ -1,5 +1,23 @@
+// legge e stampa la lista di elementi
+function getList() {
+  $.ajax({
+    url:'http://157.230.17.132:3021/todos',
+    method:'GET',
+    success:function (data) {
+      var target =$('#list')
+      for (var i = 0; i < data.length; i++) {
+        var item = data[i].text;
+        target.append('<li class="item">'+item+'</li>');
+      }
+    },
+    error:function (err) {
+      console.log(err);
+    }
+  });
+}
+
 function init() {
-  console.log('hello world');
+  getList();
 }
 
 $(document).ready(init);
